5338bbed7fa29150aa33690cdb6c23aa83aba18c
Aditya Rana <aadian0103@gmail.com>
2025-11-28 19:27:38 +0530

    bit bug

--- Data Structure/Fenw2D.cpp
+++ Data Structure/Fenw2D.cpp
@@ -8,28 +8,22 @@ public:
         init(n, m);
     }
 
-    void init(int n, int m) {
-        this->n = n;
-        this->m = m;
+    void init(int n_, int m_) {
+        n = n_;
+        m = m_;
         a.assign(n, vector<T>(m, T()));
     }
 
-    // Optimized O(n*m) build from 2D array
-    Fenwick2D(const vector<vector<T>>& b) : Fenwick2D((int)b.size(), (int)(b.empty() ? 0 : b[0].size())) {
+    Fenwick2D(const vector<vector<T>>& b) : Fenwick2D(static_cast<int>(b.size()), b.empty() ? 0 : static_cast<int>(b[0].size()))  {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                a[i][j] += b[i][j];
-                int ni = i | (i + 1);
-                int nj = j | (j + 1);
-                if (ni < n) a[ni][j] += a[i][j];
-                if (nj < m) a[i][nj] += a[i][j];
-                if (ni < n && nj < m) a[ni][nj] -= a[i][j];
+                add(i, j, b[i][j]);
             }
         }
     }
 
-    // Point update: a[x][y] += v
-    void PointUpdate(int x, int y, T v) {
+    // Add v at (x, y)
+    inline void add(int x, int y, T v) {
         assert(0 <= x && x < n && 0 <= y && y < m);
         for (int i = x + 1; i <= n; i += i & -i) {
             for (int j = y + 1; j <= m; j += j & -j) {
@@ -38,9 +32,8 @@ public:
         }
     }
 
-    // Prefix sum from (0,0) to (x,y), exclusive
-    // Sum over [0,x) x [0,y)
-    T PrefixQuery(int x, int y) {
+    // Prefix sum on [0, x) × [0, y)
+    inline T sum(int x, int y) {
         assert(0 <= x && x <= n && 0 <= y && y <= m);
         T res = T();
         for (int i = x; i > 0; i -= i & -i) {
@@ -51,15 +44,10 @@ public:
         return res;
     }
 
-    // Rectangle sum over [x1, y1) x [x2, y2) (exclusive)
-    T RectangleQuery(int x1, int y1, int x2, int y2) {
+    // Rectangle sum over [x1, y1) x [x2, y2)
+    inline T RectangleQuery(int x1, int y1, int x2, int y2) {
         assert(0 <= x1 && x1 <= x2 && x2 <= n);
         assert(0 <= y1 && y1 <= y2 && y2 <= m);
-        return PrefixQuery(x2, y2) - PrefixQuery(x1, y2) - PrefixQuery(x2, y1) + PrefixQuery(x1, y1);
-    }
-
-    // Optional: get value at (x, y)
-    T PointQuery(int x, int y) {
-        return RectangleQuery(x, y, x + 1, y + 1);
+        return sum(x2, y2) - sum(x1, y2) - sum(x2, y1) + sum(x1, y1);
     }
 };
--- Data Structure/FenwickTree.cpp
+++ Data Structure/FenwickTree.cpp
@@ -1,115 +1,95 @@
 template <typename T>
-class Fenwick {
-public:
+struct Fenwick {
     int n;
-    vector<T> a;
+    vector<T> bit;
 
     Fenwick(int n = 0) {
         init(n);
     }
 
-    void init(int n) {
-        this->n = n;
-        a.assign(n, T());
+    void init(int n_) {
+        n = n_;
+        bit.assign(n, T());
     }
 
-    // optimized build - O(n)
-    Fenwick(const vector<T> &b) : Fenwick(b.size()) {
+    // build from a vector, optimized build - O(n)
+    Fenwick(const vector<T>& b) : Fenwick(static_cast<int>(b.size())) {
         for (int i = 0; i < n; i++) {
-            a[i] += b[i];
+            bit[i] += b[i];
             int r = i | (i + 1);
-            if (r < n) {
-                a[r] += a[i];
-            }
+            if (r < n) bit[r] += bit[i];
         }
     }
 
-    void PointUpdate(int x, T v) {
-        assert(x >= 0 && x < n);
-        for (int i = x + 1; i <= n; i += i & -i) {
-            a[i - 1] += v;
+    inline void add(int x, T v) {
+        assert(0 <= x && x < n);
+        for(int i = x + 1; i <= n; i += i & -i) {
+            bit[i - 1] += v;
         }
     }
 
-    // gives prefix sum : [0,,,,i) (exclusive)
-    // for prefix sum [l, r] : RangeQuery(r + 1) - RangeQuery(l)
-    T RangeQuery(int x) {
-        assert(x >= 0 && x <= n);
+    // gives prefix sum on [0, x)
+    inline T sum(int x) const {
+        assert(0 <= x && x <= n);
         auto ans = T();
         for (int i = x; i > 0; i -= i & -i) {
-            ans += a[i - 1];
+            ans += bit[i - 1];
         }
         return ans;
     }
 
-    // Find smallest index(0 based) idx with prefix sum > k
-    // change k > a[x + i - 1] for sum >= k
-    // Returns n if no such index exists
-    int kth(T k) {
+    // gives sum on [l, r)
+    inline T rangeSum(int l, int r) const {
+        assert(0 <= l && l < r && r <= n);
+        return sum(r) - sum(l);
+    }
+
+    // smallest idx(0 based) such that prefix sum > k; returns n if none
+    int kth(T k) const {
         int x = 0;
-        for (int i = 1 << __lg(n); i; i >>= 1) {
-            if (x + i <= n && k >= a[x + i - 1]) {
+        for (int i = 1 << __lg(n); i > 0; i >>= 1) {
+            if (x + i <= n && bit[x + i - 1] <= k) {
                 x += i;
-                k -= a[x - 1];
+                k -= bit[x - 1];
             }
         }
         return x;
     }
-
-    // RUPQ implementaion
-    // add v to the range [l ,r) (exclusive) and find the value of some element
-    void RangeUpdate(int l, int r, T v) {
-        assert(l >= 0 && l <= r && r <= n);
-        PointUpdate(l, v);
-        if (r < n) {
-            PointUpdate(r, -v);
-        }
-    }
-    T PointQuery(int x) {
-        assert(x >= 0 && x < n);
-        return RangeQuery(x + 1) - RangeQuery(x);
-    }
 };
 
-template <typename T>
-class RURQ {
-private:
+// Range add, range sum
+template <class T>
+struct RURQ {
     int n;
-    Fenwick<T> a1, a2;
+    Fenwick<T> f1, f2;
 
-    void range_add(int l, int r, T v) {
-        a1.PointUpdate(l, v);
-        if (r < n) {
-            a1.PointUpdate(r, -v);
-        }
-        a2.PointUpdate(l, v * (l - 1));
-        if (r < n) {
-            a2.PointUpdate(r, -v * (r - 1));
-        }
-    }
+    RURQ(int n_) : n(n_), f1(n_), f2(n_) {}
 
-    // Sum of [0, x) (exclusive)
-    T prefix_sum(int x) {
-        return a1.RangeQuery(x) * (x - 1) - a2.RangeQuery(x);
-    }
-
-public:
-    RURQ(int size) : n(size), a1(size), a2(size) {}
-
-    RURQ(const vector<T> &a) : n((int)a.size()), a1(n), a2(n) {
+    RURQ(const vector<T>& a) : RURQ(static_cast<int>(a.size())) {
         for (int i = 0; i < n; i++) {
-            RangeUpdate(i, i + 1, a[i]);
+            range_add(i, i + 1, a[i]);
         }
     }
 
-    // Add v to range [l, r) (exclusive) and find the prefix sum in range [l, r) (exclusive)
-    void RangeUpdate(int l, int r, T v) {
-        assert(l >= 0 && l <= r && r <= n);
-        range_add(l, r, v);
+    // add v to [l, r)
+    inline void range_add(int l, int r, T v) {
+        assert(0 <= l && l < r && r <= n);
+        f1.add(l, v);
+        if (r < n) f1.add(r, -v);
+        f2.add(l, v * (l - 1));
+        if (r < n) f2.add(r, -v * (r - 1));
     }
 
-    T RangeQuery(int l, int r) {
-        assert(l >= 0 && l <= r && r <= n);
+    // prefix sum [0, x)
+    inline T prefix_sum(int x) const {
+        assert(0 <= x && x <= n);
+        return f1.sum(x) * (x - 1) - f2.sum(x);
+    }
+
+    // sum on [l, r)
+    inline T range_query(int l, int r) const {
+        assert(0 <= l && l < r && r <= n);
         return prefix_sum(r) - prefix_sum(l);
     }
 };
+
